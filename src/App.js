0c38e37bdd2e3f5f9c4ea2971afe1554c398fb7d
lej8924 <eunjae8924@naver.com>
2024-02-15 16:09:38 +0900

    plus material-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@
 import './App.css';
 import Customer from './components/Customer';
 
+import Paper from '@mui/material/Paper';
+
+import Table from '@mui/material/Table';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import { withStyles } from '@mui/material/styles';
+
+const styles = theme => ({
+  root:{
+    width : '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX : "auto"
+  },
+  table:{
+    minwidth : 200
+  }
+})
+
 const customers =[
   {
   'id':1,
@@ -27,21 +47,36 @@ const customers =[
 ]
 
 function App() {
+  const { classes } = this.props;
   return (
-    <div>
-    {
-      customers.map(c =>{
-        return <Customer 
-        key={c.id}
-        id={c.id}
-        image={c.image}
-        name={c.name}
-        age={c.age}>
-        </Customer>
-      })
-    }
-    </div>
+    <Paper className = { classes.root }>
+      <Table className = { classes.table }>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>학번</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+        customers.map(c =>{
+          return <Customer 
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          age={c.age}
+          number={c.number}>
+          </Customer>
+        })
+      }
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,20 @@
 import React from 'react';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
 
 class Customer extends React.Component{
     render(){
         return(
-        <div>
-            <CustomerProfile id={this.props.id} img={this.props.img} name={this.props.name}></CustomerProfile>
-            <CustomerInfo number ={this.props.number} age={this.props.age}></CustomerInfo>
-        </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.age}</TableCell>
+                <TableCell>{this.props.number}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.img} alt="profile">
-                </img>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.number}</p>
-                <p>{this.props.age}</p>
-            </div>
-        )
-    }
-}
 export default Customer;
